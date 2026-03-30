34be626f8e69d3dc24834c7cd1713b367de913ff
Rafet <rafetr@risk.az>
2026-03-30 20:05:38 +0400

    commit for test

--- GitTeamDemo/Program.cs
+++ GitTeamDemo/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Git Team Demo!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, From Rafat!");
 Console.WriteLine("Hello, Git Team Demo!");
